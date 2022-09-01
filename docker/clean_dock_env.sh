94e37ed7842ab35e5097ff9a3c624d3377323eed
luis.map <luismap2508@gmail.com>
2022-09-01 08:47:41 +0200

    [refac] adding cleaning of pgadmin

--- docker/clean_dock_env.sh
+++ docker/clean_dock_env.sh
@@ -3,3 +3,9 @@ docker ps -a | grep postgres-apidev | awk '{print $1}' | xargs docker container 
 
 #remove volume
 docker volume rm docker_local_pgdata
+
+#remove container for pgadmin
+docker ps -a | grep pgadmin-apidev | awk '{print $1}' | xargs docker container rm
+
+#remove volume for pgadmin
+docker volume rm  docker_pgadmin-data
