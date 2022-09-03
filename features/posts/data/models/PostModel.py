52caf498834ef7470ad0b15d3ef5399bb467e4b7
luis.map <luismap2508@gmail.com>
2022-09-03 13:05:22 +0200

    [refac] updating - post models - adding sqlalchemy

--- features/posts/data/models/PostModel.py
+++ features/posts/data/models/PostModel.py
@@ -1,5 +1,16 @@
-from asyncio.log import logger
 from features.posts.domain.entities.Post import Post
+import logging
+from core.utils.MyUtils import MyUtils
+
+appProps = MyUtils.loadProperties("general")["app"]
+logger = logging.getLogger(appProps["logger"])
 
 class PostModel(Post):
     """User post model implementation"""
+
+    def update(self, data: dict) -> Post:
+        logger = logging.getLogger(appProps["logger"])
+        for k,v in data.items():
+            logger.info(f"updating value of '{k}' from '{getattr(self, k, None)}' to '{v}'")      
+            setattr(self, k, v)
+        return self
--- features/posts/domain/entities/Post.py
+++ features/posts/domain/entities/Post.py
@@ -3,22 +3,9 @@ import logging
 from typing import Optional
 from pydantic import BaseModel
 
-from core.utils.MyUtils import MyUtils
-
-appProps = MyUtils.loadProperties("general")["app"]
-logger = logging.getLogger(appProps["logger"])
-
 class Post(BaseModel):
     id: int = -99
     title: str
     content: str
     published: bool = False
     rating: Optional[int] = None
-
-
-    def update(self, data: dict) -> Post:
-        logger = logging.getLogger(appProps["logger"])
-        for k,v in data.items():
-            logger.info(f"updating value of '{k}' from '{getattr(self, k, None)}' to '{v}'")      
-            setattr(self, k, v)
-        return self
