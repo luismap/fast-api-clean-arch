b2c592514518576fe4264ae0142b735495a16019
luis.map <luismap2508@gmail.com>
2022-12-20 08:26:29 +0100

    [feat] adding myUtils test

---
+++ test/core/utils/my_utils_test.py
@@ -0,0 +1,9 @@
+
+import pytest
+from core.utils.MyUtils import MyUtils
+
+
+def test_load_properties():
+    key = "localStore"
+    value = MyUtils.loadProperties(key)["dbFileName"]
+    assert(value == "test/post_fixtures.txt")
