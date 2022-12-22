0cdcf95f899983c62f8d28b561b2b4e89297fbb5
luis.map <luismap2508@gmail.com>
2022-12-22 16:31:39 +0100

    [refac] refactoring for mypy

--- core/utils/MyUtils.py
+++ core/utils/MyUtils.py
@@ -6,7 +6,7 @@ class MyUtils:
     """
     given a top level key, get corresponding configs
     """
-    def loadProperties(key: str) -> dict:
+    def loadProperties(self,key: str) -> dict:
         with open("properties.yaml","r") as f:
             props = yaml.safe_load(f)
             return props[key]
