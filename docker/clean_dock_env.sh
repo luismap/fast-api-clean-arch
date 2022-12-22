6bc0ac955eddcf74d56e4b8fd062cb5259c19f86
luis.map <luismap2508@gmail.com>
2022-12-22 12:37:35 +0100

    [feat] adding sqllite

--- docker/clean_dock_env.sh
+++ docker/clean_dock_env.sh
@@ -9,3 +9,9 @@ docker ps -a | grep pgadmin-apidev | awk '{print $1}' | xargs docker container r
 
 #remove volume for pgadmin
 docker volume rm  docker_pgadmin-data
+
+#remove container for sqllite
+docker ps -a | grep sqlite3 | awk '{print $1}' | xargs docker container rm
+
+#remove volume for sqllite
+docker volume rm  docker_sql_lite
