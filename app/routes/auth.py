6ba047ec2c347c16caa6764743e5ed24df40c968
luis.map <luismap2508@gmail.com>
2023-05-14 10:24:39 +0200

    [refac] uopdating auth exception to 403

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -38,13 +38,13 @@ def login(user_cred: OAuth2PasswordRequestForm = Depends()):
     if user:
         verification = MyUtils.verify(user_cred.password, user.password)
         if not verification:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
                              detail="Invalid Credentials")
         else:
             access_token = create_access_token(
                 {"user_id": user.user_id}
                 )
     else:
-        raise HTTPException(404, detail="Invalid Credentials")
+        raise HTTPException(403, detail="Invalid Credentials")
 
     return {"access_token": access_token, "token_type": "bearer"}
