65fb7e02cace19036c3ff40795b29a79c451fcf8
luis.map <luismap2508@gmail.com>
2024-02-16 14:44:14 +0100

    [refac] removing instanciation

--- test/core/utils/my_utils_test.py
+++ test/core/utils/my_utils_test.py
@@ -1,9 +1,8 @@
-
 import pytest
 from core.utils.MyUtils import MyUtils
 
 
 def test_load_properties():
     key = "localStore"
-    value = MyUtils().loadProperties(key)["dbFileName"]
-    assert(value == "test/post_fixtures.txt")
+    value = MyUtils.loadProperties(key)["dbFileName"]
+    assert value == "test/post_fixtures.txt"
