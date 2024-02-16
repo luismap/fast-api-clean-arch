f8783612c210debe9e45c2b0bb880d96bc087533
luis.map <luismap2508@gmail.com>
2024-02-16 14:43:52 +0100

    [refac] updating to ruff indenter

--- core/utils/MyUtils.py
+++ core/utils/MyUtils.py
@@ -1,28 +1,28 @@
-
-
 import yaml
 from passlib.context import CryptContext
 import os
 
 crypt_context = CryptContext(schemes="bcrypt", deprecated="auto")
 
-class MyUtils:  
+
+class MyUtils:
     def __init__(self) -> None:
         pass
+
     """
     given a top level key, get corresponding configs
     """
+
     def loadProperties(key: str) -> dict:
-        with open("properties.yaml","r") as f:
+        with open("properties.yaml", "r") as f:
             props = yaml.safe_load(f)
             return props[key]
-        
+
     def get_from_os(var: str) -> str:
         return os.getenv(var)
-    
+
     def hash(content: str) -> str:
         return crypt_context.hash(content)
 
     def verify(content: str, hashed_content: str) -> bool:
         return crypt_context.verify(content, hashed_content)
-
