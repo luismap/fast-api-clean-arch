310b8bef2f84dbf2216655a3dc9b447161a0cd11
luis.map <luismap2508@gmail.com>
2024-02-16 16:09:31 +0100

    [refac] switchin to ruff indenter and closing a file

--- core/db/LocalStore.py
+++ core/db/LocalStore.py
@@ -1,4 +1,3 @@
-
 from asyncio.log import logger
 import json
 from logging import Logger
@@ -11,36 +10,35 @@ from core.utils.MyUtils import MyUtils
 Interact with local files
 Helper class mainly for testing purpose
 """
-class LocalStore:
 
+
+class LocalStore:
     def __init__(self) -> None:
         self.appProps = MyUtils.loadProperties("general")["app"]
         self.logger = logging.getLogger(self.appProps["logger"])
 
     def isAvailable(self, file: str) -> bool:
         try:
-            f = open(file, 'r')
+            f = open(file, "r")
             self.logger.info("local db available")
+            f.close()
             return True
         except Exception as ex:
             self.logger.info(f"problem reading file: {ex}")
             return False
 
-
-
-    def getLocalData(self,file: str) -> Optional[list[dict]]:
+    def getLocalData(self, file: str) -> Optional[list[dict]]:
         try:
             with open(file, "r") as f:
                 ans = json.load(f)["data"]
         except:
-                ans = None
+            ans = None
         return ans
-    
+
     def dumpLocalData(self, file: str, posts: list[dict]) -> bool:
         try:
             with open(file, "w") as f:
-                json.dump({"data": [e for e in posts]},f)
+                json.dump({"data": [e for e in posts]}, f)
             return True
         except Exception as e:
             return False
-
