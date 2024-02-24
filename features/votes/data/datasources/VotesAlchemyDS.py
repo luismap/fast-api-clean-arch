b0ea1938795f1f2c85b0b88efcacae0a2bf7b330
luis.map <luismap2508@gmail.com>
2024-02-24 12:59:57 +0100

    [refac] adding ruff indenter

--- features/votes/data/datasources/VotesAlchemyDS.py
+++ features/votes/data/datasources/VotesAlchemyDS.py
@@ -1,4 +1,3 @@
-
 import logging
 from typing import List
 
@@ -8,11 +7,14 @@ from core.db.AlchemySql import Base, SqlAlchemyAccessLayer
 from core.db.Postgres import PostgresConn
 from core.db.models.AlchemyModels import Votes
 from features.votes.data.datasources.api.VotesDataSource import VotesDataSource
-from features.votes.data.models.VotesModel import VoteModel, VoteReadModel, VoteResponseModel
+from features.votes.data.models.VotesModel import (
+    VoteModel,
+    VoteReadModel,
+    VoteResponseModel,
+)
 
 
 class VotesAlchemyDS(VotesDataSource):
-
     def __init__(self, postgres_conn: PostgresConn) -> None:
         self.sqlal = SqlAlchemyAccessLayer(postgres_conn.get_uri_conn())
         Base.metadata.create_all(bind=self.sqlal.engine)
@@ -29,50 +31,58 @@ class VotesAlchemyDS(VotesDataSource):
         with self.SessionLocal() as session:
             try:
                 session.query(Votes).filter(
-                    Votes.user_id==user_id, Votes.post_id==post_id
+                    Votes.user_id == user_id, Votes.post_id == post_id
                 ).delete()
                 session.commit()
                 return True
             except Exception as e:
                 logging.info(e)
                 return False
-    
+
     def get_my_votes(self, user_id: int) -> List[VoteReadModel]:
         with self.SessionLocal() as session:
             votes = session.query(Votes).filter(Votes.user_id == user_id).all()
         return votes
-    
-    def create_vote(self,vote:VoteModel)-> VoteReadModel:
+
+    def create_vote(self, vote: VoteModel) -> VoteReadModel:
         with self.SessionLocal() as session:
             try:
                 session.add(Votes(**vote.dict()))
                 session.commit()
             except Exception as e:
                 logging.info(e)
-                raise HTTPException(status.HTTP_409_CONFLICT, "conflict while adding vote")
+                raise HTTPException(
+                    status.HTTP_409_CONFLICT, "conflict while adding vote"
+                )
 
         return vote
-    
+
     def vote(self, post_id: int, user_id: int, direction: bool) -> VoteReadModel:
         find_post = lambda vote: vote.post_id == post_id
         voted = list(filter(find_post, self.get_my_votes(user_id)))
         vote_model = VoteModel.from_orm(voted[0]) if voted else None
         logging.info(f"found vote {vote_model}")
         ans = None
-        if(direction):
+        if direction:
             logging.info("upvoting")
-            if (not vote_model):
+            if not vote_model:
                 logging.info("creating vote")
                 ans = self.create_vote(VoteModel(user_id=user_id, post_id=post_id))
             else:
-                raise HTTPException(status.HTTP_409_CONFLICT, f"vote already exist for post_id {post_id}")
+                raise HTTPException(
+                    status.HTTP_409_CONFLICT,
+                    f"vote already exist for post_id {post_id}",
+                )
         else:
             logging.info("downvoting")
-            if (not vote_model):
-                raise HTTPException(status.HTTP_404_NOT_FOUND, f"post_id {post_id} not found for user {user_id}")
+            if not vote_model:
+                raise HTTPException(
+                    status.HTTP_404_NOT_FOUND,
+                    f"post_id {post_id} not found for user {user_id}",
+                )
             else:
                 deleted = self.delete_vote(post_id, user_id)
-                if (deleted):
+                if deleted:
                     ans = VoteReadModel(user_id=user_id, post_id=post_id)
 
         return ans
