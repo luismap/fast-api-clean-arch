4b049d99bf1e733dd7ebf5ff8de464c42084c17a
luis.map <luismap2508@gmail.com>
2024-02-24 12:12:40 +0100

    [refac] switching to ruff indenter

--- features/posts/domain/controllers/PostsController.py
+++ features/posts/domain/controllers/PostsController.py
@@ -7,36 +7,33 @@ from features.posts.domain.entities.Post import PostCreate, PostRead
 
 class PostController(ABC):
     @abstractmethod
-    def getPosts(self,
-                limit: int,
-                offset: int,
-                search_title: Optional[str]) -> Optional[List[PostModel]]:
+    def getPosts(
+        self, limit: int, offset: int, search_title: Optional[str]
+    ) -> Optional[List[PostModel]]:
         pass
 
     @abstractmethod
-    def dumpPosts(self,posts: List[PostCreate]):
+    def dumpPosts(self, posts: List[PostCreate]):
         pass
 
     @abstractmethod
-    def getPost(self,id: int) -> PostRead:
+    def getPost(self, id: int) -> PostRead:
         pass
 
     @abstractmethod
-    def createPost(self,post: PostCreateModel) -> bool:
+    def createPost(self, post: PostCreateModel) -> bool:
         pass
 
     @abstractmethod
-    def updatePost(self,id: int, post: dict, as_user: int) -> bool:
+    def updatePost(self, id: int, post: dict, as_user: int) -> bool:
         pass
 
     @abstractmethod
-    def deletePost(self,postId: int, as_user: int ) -> Optional[PostModel]:
+    def deletePost(self, postId: int, as_user: int) -> Optional[PostModel]:
         pass
 
     @classmethod
     def get_post_by_user(
-        as_user: int,
-        limit: int,
-        offset: int,
-        search_title: Optional[str]    ) -> List[PostRead]:
+        as_user: int, limit: int, offset: int, search_title: Optional[str]
+    ) -> List[PostRead]:
         pass
