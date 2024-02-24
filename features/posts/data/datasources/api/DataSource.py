d24beeb788bca334a7ce0e6804769dd3cd505dfe
luis.map <luismap2508@gmail.com>
2024-02-24 12:40:39 +0100

    [refac] switching indenter

--- features/posts/data/datasources/api/DataSource.py
+++ features/posts/data/datasources/api/DataSource.py
@@ -1,45 +1,39 @@
-
 from abc import ABC, abstractmethod
 from typing import Optional
 from features.posts.data.models.PostCreateModel import PostCreateModel
 from features.posts.data.models.PostModel import PostModel
 from features.posts.domain.entities.Post import PostCreate, PostRead
 
-class DataSource(ABC):
 
+class DataSource(ABC):
     @classmethod
     def isAvailable(self) -> bool:
         pass
 
     @classmethod
-    def getPosts(self,
-                limit: int,
-                offset: int) -> Optional[list[PostRead]]:
+    def getPosts(self, limit: int, offset: int) -> Optional[list[PostRead]]:
         pass
 
     @classmethod
-    def dumpPosts(self,posts: list[PostCreateModel]):
-        pass
-    
-    @classmethod
-    def getPost(self,id: int) -> Optional[PostRead]:
+    def dumpPosts(self, posts: list[PostCreateModel]):
         pass
 
     @classmethod
-    def createPost(self,post: PostCreateModel) -> bool:
+    def getPost(self, id: int) -> Optional[PostRead]:
         pass
 
     @classmethod
-    def updatePost(self,id: int, post: dict, as_user: int) -> bool:
+    def createPost(self, post: PostCreateModel) -> bool:
         pass
 
     @classmethod
-    def deletePost(self,postId: int, as_user: int ) -> Optional[PostModel]:
+    def updatePost(self, id: int, post: dict, as_user: int) -> bool:
         pass
 
     @classmethod
-    def getPostByUser(
-        user_id: int,
-        limit: int,
-        offset: int) -> list[PostRead]:
+    def deletePost(self, postId: int, as_user: int) -> Optional[PostModel]:
+        pass
+
+    @classmethod
+    def getPostByUser(user_id: int, limit: int, offset: int) -> list[PostRead]:
         pass
