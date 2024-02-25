72012d890a0878e4e4cd757ebdea98eec01378bf
luis.map <luismap2508@gmail.com>
2024-02-25 11:16:02 +0100

    [refac] updating to ruff indenter

--- core/db/AlchemySql.py
+++ core/db/AlchemySql.py
@@ -3,10 +3,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+
 class SqlAlchemyAccessLayer:
     def __init__(self, conn: str) -> None:
         self.sqlAlchemyDatabaseUrl = conn
         self.engine = create_engine(self.sqlAlchemyDatabaseUrl)
-        self.SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
+        self.SessionLocal = sessionmaker(
+            autocommit=False, autoflush=False, bind=self.engine
+        )
+
 
 Base = declarative_base()
