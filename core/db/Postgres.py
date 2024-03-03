86bcbdf35d834fa67817c386d19299aa56c5206b
luis.map <luismap2508@gmail.com>
2024-03-03 10:58:27 +0100

    [refac] switching to ruff indenter

--- core/db/Postgres.py
+++ core/db/Postgres.py
@@ -6,16 +6,17 @@ import psycopg
 """
 Get common details for postgres db
 """
+
+
 class PostgresConn:
-    def get_conn_params(self) -> Dict[str,str]:
+    def get_conn_params(self) -> Dict[str, str]:
         pg_uri = self.get_uri_conn()
         return psycopg.conninfo.conninfo_to_dict(pg_uri)
 
     def get_uri_conn(self) -> str:
         load_dotenv()
-        pguser=os.getenv('pguser')
-        pgpass=os.getenv('pgpass')
-        pgdbname=os.getenv('pgdbname')
-        pghost=os.getenv('pghost')
+        pguser = os.getenv("pguser")
+        pgpass = os.getenv("pgpass")
+        pgdbname = os.getenv("pgdbname")
+        pghost = os.getenv("pghost")
         return f"postgresql://{pguser}:{pgpass}@{pghost}/{pgdbname}"
-
