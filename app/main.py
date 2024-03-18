7c10c1ebbd1e420675b42ef5eac51c4482ddc9b6
luis.map <luismap2508@gmail.com>
2024-03-18 16:38:04 +0100

    [feat] adding basic cors

--- app/main.py
+++ app/main.py
@@ -1,21 +1,35 @@
-from fastapi import Body, FastAPI
-
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import logging
 import logging.config
 from app.routes import auth, post, user, votes
 from core.utils.MyUtils import MyUtils
-from features.user.data.controllers.UserHandler import UserHandler
-    
+
 canlog = True
 appProps = MyUtils.loadProperties("general")["app"]
 logger = logging.getLogger(appProps["logger"])
 
 app = FastAPI()
 
+origins = [
+    "https://mydomain.com",  # useful for cross-origin resource sharin
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["get", "post"],
+    allow_headers=["*"],
+)
+
+
 @app.get("/")
 def read_root():
-    if canlog: logger.info("root got call")
+    if canlog:
+        logger.info("root got call")
     return {"Hello": "World"}
+
 
 app.include_router(post.router)
 app.include_router(user.router)
